8fd509efe0bb8b98fc6744b0b5177a80237968e9
xwang2014 <x.wang2014@gmail.com>
2015-03-14 00:53:56 -0700

    LintCode strstr

---
+++ src/lintcode/strstr.java
@@ -0,0 +1,38 @@
+package lintcode;
+
+public class strstr {
+	
+    /**
+     * Returns a index to the first occurrence of target in source, or -1  if target is not part of source.
+     * @param source string to be scanned.
+     * @param target string containing the sequence of characters to match.
+     */
+    public int strStr(String source, String target) {
+        if (source == null || target == null) return -1;
+        if (source.length() < target.length()) return -1;
+        
+        for(int i = 0; i <= source.length() - target.length(); i++) {
+            boolean flag = true;
+            
+            for(int j = 0; j < target.length(); j++) {
+                if(target.charAt(j) != source.charAt(i + j))
+                {
+                    flag = false;
+                    break;
+                }    
+            }
+            
+            if(flag) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
